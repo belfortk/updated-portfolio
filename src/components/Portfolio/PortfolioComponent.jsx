67afe1626a9fd048ab3f399c640f248288f56a06
kyle <belfortk@gmail.com>
2018-01-03 16:18:34 -0800

    final touch ups. first draft done.

--- src/components/Portfolio/PortfolioComponent.jsx
+++ src/components/Portfolio/PortfolioComponent.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Footer from "../Footer";
 
 class PortfolioItem extends React.Component {
-
   render() {
     return (
       <div>
@@ -11,22 +10,33 @@ class PortfolioItem extends React.Component {
             <h3> {this.props.title}</h3>
             <p> {this.props.description}</p>
             <a href={this.props.liveLink}>See it live</a> <br />
-            {this.props.github ?         <a href={this.props.github}><img style={{"position": "absolute", "top": 0, "right": 0, "border": 0}} src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Check me out on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></img></a>
-: <p style={{"position": "absolute", "top": 0, "right": 0, "border": 0, "marginRight": "1rem" }}>Sorry, this is a private repo! </p>}
-
-            <div className="project-hero-image z-depth-5">
-              <img className="responsive-img" src={this.props.heroImg} alt="project hero screenshot" />
-            </div>
+            {this.props.github ? (
+              <a href={this.props.github}>
+                <img
+                  style={{ position: "absolute", top: 0, right: 0, border: 0 }}
+                  src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67"
+                  alt="Check me out on GitHub"
+                />
+              </a>
+            ) : (
+              <p style={{ position: "absolute", top: 0, right: 0, border: 0, marginRight: "1rem" }}>
+                Sorry, this is a private repo!{" "}
+              </p>
+            )}
+            <a href={this.props.liveLink}>
+              <img className="responsive-img z-depth-5 grow" width="600px" height="400px" src={this.props.heroImg} alt="project hero screenshot" />
+            </a>
+          
             <br />
             <br />
             <h5>Summary</h5>
             <p>{this.props.summary}</p>
-            <div className="row" id='supporting-images-row'>
+            <div className="row" id="supporting-images-row">
               <div className="project-supporting-image col m5 s12" id="supp-img-1">
-                <img src={this.props.supportingImg1} alt="" />
+                <img width="500" height="300" src={this.props.supportingImg1} alt="" />
               </div>
-              <div className="project-supporting-image col m5 s12" id='supp-img-2'>
-                <img src={this.props.supportingImg2} alt="" />
+              <div className="project-supporting-image col m5 s12" id="supp-img-2">
+                <img width="500" height="300" src={this.props.supportingImg2} alt="" />
               </div>
             </div>
           </div>
